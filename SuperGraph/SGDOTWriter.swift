87ecb1e7b509dec78a4e8c96949053f66560fa34
Alexandre Lopoukhine <superlopuh@gmail.com>
2015-04-17 13:34:11 +0100

    added a function to write graph to SGDOTWriter

--- SuperGraph/SGDOTWriter.swift
+++ SuperGraph/SGDOTWriter.swift
@@ -9,5 +9,25 @@
 import Foundation
 
 public class SGDOTWriter {
-    
+    public static func writeGraph<N, E: Hashable>(graph: SGGraph<N,E>, withName name: String, toFile outputFileAddress: NSURL) {
+        // If folder doesn't exist, create one
+        if let folderPath = outputFileAddress.URLByDeletingLastPathComponent?.path {
+            // check if element is a directory
+            var isDirectory: ObjCBool = ObjCBool(false)
+            // Mutates isDirectory pointer
+            NSFileManager.defaultManager().fileExistsAtPath(folderPath, isDirectory: &isDirectory)
+            
+            if !isDirectory {
+                var error: NSError?
+                
+                NSFileManager.defaultManager().createDirectoryAtPath(folderPath, withIntermediateDirectories: false, attributes: nil, error: &error)
+            }
+        }
+        
+        // Create new .dot file
+        if let dotFilePath = outputFileAddress.path {
+            let dotData = graph.dotDescriptionWithName(name).dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: false)
+            NSFileManager.defaultManager().createFileAtPath(dotFilePath, contents: dotData, attributes: nil)
+        }
+    }
 }
