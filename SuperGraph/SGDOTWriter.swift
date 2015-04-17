971942c550f1ecf90d7700b6a86b155323f148cd
Alexandre Lopoukhine <superlopuh@gmail.com>
2015-04-17 13:20:45 +0100

    created public SGDOTWriter class

---
+++ SuperGraph/SGDOTWriter.swift
@@ -0,0 +1,13 @@
+//
+//  SGDOTWriter.swift
+//  SuperGraph
+//
+//  Created by Alexandre Lopoukhine on 17/04/2015.
+//  Copyright (c) 2015 bluetatami. All rights reserved.
+//
+
+import Foundation
+
+public class SGDOTWriter {
+    
+}
